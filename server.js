b8cb111e288abc09147c17a3dbff051a630f615b
maxcucaro1977 <maxplanet2013@gmail.com>
2025-04-21 20:21:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,27 @@ const io = new Server(server, {
   pingInterval: 25000
 });
 
+// Servi i file statici dalla cartella dist
+app.use(express.static(join(__dirname, 'dist')));
+
 io.on('connection', (socket) => {
   console.log('Client connesso:', socket.id);
 
   socket.on('stream-data', (data) => {
     socket.broadcast.emit('stream-data', data.stream);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnesso:', socket.id);
+  });
+});
+
+// Gestisci tutte le altre richieste reindirizzandole a index.html
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server avviato sulla porta ${PORT}`);
+});
