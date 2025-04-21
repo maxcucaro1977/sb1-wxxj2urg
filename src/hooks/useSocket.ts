3f878709eaa1e40bcb6258c3d102b96057469995
maxcucaro1977 <maxplanet2013@gmail.com>
2025-04-21 16:10:02 +0200

    Update useSocket.ts

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.DEV 
-  ? window.location.hostname === 'localhost'
-    ? 'http://localhost:10000'
-    : `http://${window.location.hostname}:10000`
+  ? `${window.location.protocol}//${window.location.host}`
   : 'https://armony.onrender.com';
 
 export const useSocket = () => {
@@ -20,48 +18,6 @@ export const useSocket = () => {
       reconnectionAttempts: 5,
       reconnectionDelay: 1000,
       timeout: 10000,
-      autoConnect: false
+      autoConnect: false,
+      withCredentials: false
     });
-
-    newSocket.connect();
-
-    newSocket.on('connect', () => {
-      setIsConnected(true);
-      setIsConnecting(false);
-      setError(null);
-      setReconnectAttempts(0);
-    });
-
-    newSocket.on('connect_error', (err) => {
-      setIsConnected(false);
-      setIsConnecting(false);
-      setError(`Errore di connessione: ${err.message}`);
-      
-      if (reconnectAttempts < 5) {
-        setTimeout(() => {
-          setReconnectAttempts(prev => prev + 1);
-          newSocket.connect();
-        }, 1000 * Math.pow(2, reconnectAttempts));
-      }
-    });
-
-    newSocket.on('disconnect', () => {
-      setIsConnected(false);
-      setError('Disconnesso dal server');
-    });
-
-    setSocket(newSocket);
-
-    return () => {
-      newSocket.disconnect();
-    };
-  }, [reconnectAttempts]);
-
-  return {
-    socket,
-    isConnected,
-    isConnecting,
-    error,
-    reconnectAttempts
-  };
-};
