9e8c529a5049353440ed2f34da0db31058f5eeae
maxcucaro1977 <maxplanet2013@gmail.com>
2025-04-21 20:07:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,16 +13,14 @@ const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
-  }
+  },
+  transports: ['websocket', 'polling'],
+  pingTimeout: 60000,
+  pingInterval: 25000
 });
 
 io.on('connection', (socket) => {
+  console.log('Client connesso:', socket.id);
+
   socket.on('stream-data', (data) => {
     socket.broadcast.emit('stream-data', data.stream);
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server avviato sulla porta ${PORT}`);
-});
